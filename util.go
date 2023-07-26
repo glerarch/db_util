ff395a133cfc9646ab72af9674490b3d431b0cdd
c3nsored <u.gabrielr@gmail.com>
2023-07-26 21:26:57 +0200

    fix

--- util.go
+++ util.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/cenkalti/backoff"
@@ -25,6 +26,7 @@ type DBConfig struct {
 	Port     string
 	DBName   string
 }
+type Operation func(ctx context.Context, ext sqlx.ExtContext) error
 
 func (db PostgresDB) Connect(cfg *DBConfig) (*sql.DB, error) {
 	if cfg == nil {
@@ -34,7 +36,23 @@ func (db PostgresDB) Connect(cfg *DBConfig) (*sql.DB, error) {
 	return sql.Open("postgres", psqlURL)
 }
 
-type Operation func(ctx context.Context, ext sqlx.ExtContext) error
+func WithRetry(ctx context.Context, db *sqlx.DB, query string, fn Operation) error {
+	var err error
+	retry := 0
+
+	err = backoff.Retry(func() error {
+		retry++
+		if err := doOnce(ctx, fn, db); err != nil {
+			log.Println("recieved err")
+			return backoff.Permanent(err)
+		}
+		return nil
+	}, backoff.WithContext(newExponentialBackOff(), ctx))
+	if err == nil {
+		return nil
+	}
+	return err
+}
 
 func doOnce(ctx context.Context, op Operation, db *sqlx.DB) error {
 	tx, err := db.BeginTxx(context.Background(), &sql.TxOptions{
